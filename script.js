150294aa1f8c5f8782772cb914881eaad37b42bc
lovegupta112 <loveguptaofficials@gmail.com>
2023-07-07 19:59:01 +0530

    added functionality

--- script.js
+++ script.js
@@ -9,29 +9,70 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
+
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn=document.getElementById('clear-cart-btn');
+
+let carts=JSON.parse(sessionStorage.getItem('carts')) || [];
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}" onclick="addToCart(${product.id})">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
+clearCartBtn.addEventListener('click',clearCart);
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+ if(carts){
+  cartList.innerHTML="";
+  carts.forEach((product)=>{
+    const li=document.createElement('li');
+    li.innerHTML=`${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}" onclick="removeFromCart(${product.id})">Remove from Cart</button>`;
+    cartList.appendChild(li);
+  })
+ }else{
+  cartList.innerHTML="";
+ }
+}
+
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+
+ let product=products.find((product)=>{
+    if(product.id==productId)
+       return product;
+  })
+  carts.push(product);
+  sessionStorage.setItem('carts',JSON.stringify(carts));
+  renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+// console.log(productId)
+if(carts){
+     carts=carts.filter((product)=>{
+        if(product.id!=productId)
+        return product;
+ })
+ renderCart();
+ sessionStorage.setItem('carts',JSON.stringify(carts));
+}
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  sessionStorage.removeItem('carts');
+  carts=[];
+  renderCart();
+}
 
 // Initial render
 renderProducts();
